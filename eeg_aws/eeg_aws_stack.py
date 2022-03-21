487cff77ba93029a3cf4bfe7b9e8d69b92810c31
Will Robbins <williamrbs1996@gmail.com>
2022-03-21 08:44:23 -0300

    Can convert raw data to bids

--- eeg_aws/eeg_aws_stack.py
+++ eeg_aws/eeg_aws_stack.py
@@ -1,11 +1,17 @@
 from constructs import Construct
 from aws_cdk import (
     Stack,
+    Size,
+    Duration,
+    RemovalPolicy,
+    CfnOutput,
+    custom_resources as cr,
     aws_iam as iam,
-    aws_s3 as s3,
+    aws_apigateway as apigw,
     aws_lambda as _lambda,
-    aws_s3_notifications as s3_notify,
-    aws_lambda_event_sources as events
+    aws_ec2 as ec2,
+    aws_efs as efs,
+    aws_codebuild as codebuild
 )
 
 
@@ -14,24 +20,96 @@ class EegAwsStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        group = iam.Group(self, 'eeg-test-group')
-        user = iam.User(self, 'eeg-test-user')
-        user.add_to_group(group)
+        vpc = ec2.Vpc(self, 'eeg-test-vpc', max_azs=2, nat_gateways=1)
 
-        bucket = s3.Bucket(self, 'eeg-test-raw-bucket')
-        bucket.grant_read_write(user)
+        ec2SecurityGroup = ec2.SecurityGroup(self, 'eeg-test-ec2SG', vpc=vpc, security_group_name='eeg-test-ec2SG')
+        lambdaSecurityGroup = ec2.SecurityGroup(self, 'eeg-test-lambdaSG', vpc=vpc, security_group_name='eeg-test-lambdaSG')
+        efsSecurityGroup = ec2.SecurityGroup(self, 'eeg-test-efsSG', vpc=vpc, security_group_name='eeg-test-efsSG')
 
-        lambda_func = _lambda.Function(self, 'eeg-test-lambdaListener',
-                                       runtime=_lambda.Runtime.PYTHON_3_8,
-                                       handler='LambdaListener.handler',
-                                       code=_lambda.Code.from_asset('lambda'),
-                                       environment={'BUCKET_NAME': bucket.bucket_name})
+        ec2SecurityGroup.connections.allow_to(efsSecurityGroup, ec2.Port.tcp(2049))
+        lambdaSecurityGroup.connections.allow_to(efsSecurityGroup, ec2.Port.tcp(2049))
 
-        notification = s3_notify.LambdaDestination(lambda_func)
-        notification.bind(self, bucket)
+        fs = efs.FileSystem(self, 'eeg-test-efs',
+                            vpc=vpc,
+                            security_group=efsSecurityGroup,
+                            throughput_mode=efs.ThroughputMode.PROVISIONED,
+                            provisioned_throughput_per_second=Size.mebibytes(10),
+                            removal_policy=RemovalPolicy.DESTROY)
 
-        # trigger lambda when brain-imaging files are added
-        s3PutEventSource = events.S3EventSource(bucket,
-                                                events=[s3.EventType.OBJECT_CREATED])
-        lambda_func.add_event_source(s3PutEventSource)
+        efsAccessPoint = efs.AccessPoint(self, 'eeg-test-accesspoint',
+                                         file_system=fs,
+                                         path='/lambda',
+                                         posix_user=efs.PosixUser(gid='1000', uid='1000'),
+                                         create_acl=efs.Acl(owner_gid='1000', owner_uid='1000', permissions='777'))
 
+        executeBidsConverter = _lambda.Function(self, 'eeg-test-lambdaBidsConverter',
+                                                runtime=_lambda.Runtime.PYTHON_3_8,
+                                                handler='bids.lambda_handler',
+                                                code=_lambda.Code.from_asset("./lambda"),
+                                                vpc=vpc,
+                                                vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT),
+                                                security_groups=[lambdaSecurityGroup],
+                                                timeout=Duration.minutes(2),
+                                                memory_size=4096,
+                                                reserved_concurrent_executions=10,
+                                                filesystem=_lambda.FileSystem.from_efs_access_point(efsAccessPoint, '/mnt/python'))
+        executeBidsConverter.role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonElasticFileSystemClientFullAccess"))
+
+        codeBuildProject = codebuild.Project(self, 'eeg-test-codebuildProject',
+                                             project_name="eeg-test-codebuildProject",
+                                             description="Installs Python libraries to EFS.",
+                                             vpc=vpc,
+                                             build_spec=codebuild.BuildSpec.from_object({
+                                                 'version': '0.1',
+                                                 'phases': {
+                                                     'build': {
+                                                         'commands': [
+                                                             'echo "Installing virtual environment..."',
+                                                             'mkdir -p $CODEBUILD_EFS1/lambda',
+                                                             'echo "Clearing previous environment, if it exists..."',
+                                                             'rm -rf $CODEBUILD_EFS1/lambda/mne',
+                                                             'python3 -m venv $CODEBUILD_EFS1/lambda/mne',
+                                                             'source $CODEBUILD_EFS1/lambda/mne/bin/activate && pip3 install mne && pip3 install mne-bids',
+                                                             'echo "Changing folder permissions..."',
+                                                             'chown -R 1000:1000 $CODEBUILD_EFS1/lambda/'
+                                                         ]
+                                                     }
+                                                 }
+                                             }),
+                                             environment=codebuild.BuildEnvironment(build_image=codebuild.LinuxBuildImage.from_docker_registry('lambci/lambda:build-python3.8'),
+                                                                                    compute_type=codebuild.ComputeType.LARGE,
+                                                                                    privileged=True),
+                                             security_groups=[ec2SecurityGroup],
+                                             subnet_selection=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT),
+                                             timeout=Duration.minutes(30))
+
+        cfnProject: codebuild.CfnProject
+        cfnProject = codeBuildProject.node.default_child
+        cfnProject.file_system_locations = [codebuild.CfnProject.ProjectFileSystemLocationProperty(
+            identifier='efs1',
+            location=fs.file_system_id + ".efs." + Stack.of(self).region + ".amazonaws.com:/",
+            mount_point='/mnt/python',
+            type='EFS',
+            mount_options="nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2"
+        )]
+        cfnProject.logs_config = {
+            "cloudWatchLogs": {
+                "status": "ENABLED"
+            }
+        }
+
+        # Trigger the CodeBuild project to install Python packages to the EFS
+        triggerBuildProject = cr.AwsCustomResource(self, 'eeg-test-triggerCodeBuild',
+                                                   on_create=cr.AwsSdkCall(service='CodeBuild',
+                                                                           action='startBuild',
+                                                                           parameters={
+                                                                               "projectName": codeBuildProject.project_name
+                                                                           },
+                                                                           physical_resource_id=cr.PhysicalResourceId.from_response('build.id')),
+                                                   policy=cr.AwsCustomResourcePolicy.from_sdk_calls(
+                                                       resources=cr.AwsCustomResourcePolicy.ANY_RESOURCE
+                                                   ))
+
+        codeBuildProject.node.add_dependency(efsAccessPoint)
+
+        CfnOutput(self, 'LambdaFunctionName', value=executeBidsConverter.function_name)
---
+++ lambda/bids.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+efs_path = '/mnt/python'
+python_pkg_path = os.path.join(efs_path, "mne/lib/python3.8/site-packages")
+sys.path.append(python_pkg_path)
+
+import mne
+import json
+from mne_bids import BIDSPath, write_raw_bids
+
+
+def lambda_handler(event, context):
+
+    sample_data_raw_file = os.path.join(efs_path, 'sub_01', 'sample_audvis_filt-0-40_raw.fif')
+    raw = mne.io.read_raw_fif(sample_data_raw_file)
+
+    bids_dataset_path = os.path.join(efs_path, 'bids_dataset')
+    bids_path = BIDSPath(subject='01', session='01', run=5,
+                         datatype='meg', task='testtask', root=bids_dataset_path)
+    write_raw_bids(raw, bids_path=bids_path)
+
+    return {
+        'statusCode': 200,
+        'body': 'write_raw_bids test'
+    }
